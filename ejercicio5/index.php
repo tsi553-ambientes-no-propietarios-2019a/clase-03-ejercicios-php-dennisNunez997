fa3ac62513919d7a8c4bf14d854b4fad30076437
dennis <dennis.nunez@epn.edu.ec>
2019-04-22 22:55:39 -0500

    ejercicio terminado

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -12,4 +12,39 @@ Para imprimir el contenido de una variable también utilizamos el comando echo:
 
 echo $num;
  */
+
+
 ?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>registro</title>
+        <meta charset= "UTF-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    </head>
+
+    <body>
+        <div class = "container">
+            <form action = "aleatorio.php" method = "POST">
+                <h1 class = 'page-header'>Obtener un numero aleatorio</h1>
+
+                <div class= "form-group">
+                    <label>Presione el boton "ALEATORIO"</label>
+                    
+                </div>
+
+                <div class = "form-group">
+                    
+                    <button class="btn btn-primary" type="submit">Aleatorio</button>
+
+                </div>
+            </form>
+        </div>
+    </body>
+
+</html>
